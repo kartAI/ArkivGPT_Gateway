9d85dbc39823c4806b6bd08c0aa4060dceaeb236
Sondre Horpestad <sondreho@uia.no>
2024-02-06 14:49:09 +0100

    Added grpc support

--- ArkivGPT/Controllers/SummaryController.cs
+++ ArkivGPT/Controllers/SummaryController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
 
 namespace ArkivGPT.Controllers;
 
@@ -18,6 +19,13 @@ public class SummaryController : ControllerBase
     public async IAsyncEnumerable<SummaryElement> Get([FromQuery] int gnr, [FromQuery] int bnr, [FromQuery] int snr)
     {
         var element = new SummaryElement(new DateOnly(2020, 5, 2), "Test resolution 1", "http://test1");
+
+        // Use this to send the data were it is supposed to go
+        var serverAddress = Environment.GetEnvironmentVariable("GRPC_SERVER_ADDRESS") ?? "http://localhost:5001"; // To handel docker and local
+        using var channel = GrpcChannel.ForAddress(serverAddress);
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient123" });
+        Console.WriteLine("Greeting: " + reply.Message);
 
         yield return element;
     }
